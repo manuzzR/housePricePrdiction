2ce48864bfbdae32aa3db493d2b6186071c5a083
Manu <76021656+manuzzR@users.noreply.github.com>
2024-09-28 20:15:24 -0600

    Update dataSplit.py

--- dataSplit.py
+++ dataSplit.py
@@ -1,2 +1,2 @@
 #Let split the data into train and test data set. We will use housing data and sklearn
-housing["housing_cat"] = np.ceil(housing 
+housing["housing_cat"] = np.ceil(housing["median_income"]/1.5]
